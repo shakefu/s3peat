2d7f699800e5aa908eabc34ab0f59d2976a41ad8
shakefu <shakefu@gmail.com>
2014-01-10 00:44:02 -0800

    Refactor all the things and add command line scripts.

--- s3peat/__init__.py
+++ s3peat/__init__.py
@@ -29,7 +29,7 @@ from threading import Thread
 import boto
 
 
-_log = logging.getLogger(__name__)
+__version__ = '0.1.2'
 
 
 class S3Bucket(object):
@@ -70,14 +70,20 @@ class S3Queue(Thread):
     """
     Take a list of `filenames` and upload them to S3 with leading key `prefix`.
 
+    If `counter` is specified, it will be called once with no arguments for
+    each successful upload, and once with ``False`` as its only argument for
+    each failed upload.
+
     :param prefix: S3 key prefix
     :param filenames: Iterable of filenames
     :param bucket: A :class:`S3Bucket` instance
     :param strip_path: Leading path to strip (optional)
+    :param counter: This is called once for each upload (optional)
     :type prefix: str
     :type filenames: list
     :type bucket: :class:`S3Bucket`
     :type strip_path: str
+    :type counter: callable
 
     If `strip_path` is specified, `strip_path` will be stripped from the front
     of each filename before composing the uploaded key.
@@ -94,6 +100,9 @@ class S3Queue(Thread):
 
     """
     def __init__(self, prefix, filenames, bucket, strip_path=None, **kwargs):
+        # Get the counting callback if it's set
+        self.counter = kwargs.pop('counter', None)
+
         kwargs.setdefault('name', "S3Queue.{}:{}".format(bucket, id(self)))
 
         super(S3Queue, self).__init__(**kwargs)
@@ -135,11 +144,14 @@ class S3Queue(Thread):
             else:
                 s3key.set_acl('authenticated-read')
         except:
-            self.log.error("Failed %r", key)
-            self.log.debug("Oops!", exc_info=True)
+            self.log.debug("Failed %r", key, exc_info=True)
             self.failed.append(filename)
+            if self.counter:
+                self.counter(False)
         else:
             self.log.debug("Uploaded %r", key)
+            if self.counter:
+                self.counter()
 
     def _key(self, filename):
         """
@@ -162,49 +174,183 @@ class S3Queue(Thread):
         return self.name
 
 
-def sync_to_s3(directory, prefix, bucket, include=None, exclude=None,
-        concurrency=1):
+class S3Uploader(object):
     """
-    Uploads `directory` to S3 with `prefix` to the key names in `bucket`.
+    Runs a set of parallel uploads.
 
     :param directory: Directory to sync
     :param prefix: S3 key prefix
     :param bucket: A :class:`S3Bucket` instance
-    :param exclude: A filename regex to exclude (optional)
-    :param include: A filename regex to include (optional)
+    :param exclude: List of filename regexes to exclude (optional)
+    :param include: List of filename regexes to include (optional)
     :param concurrency: Number of concurrent uploads to use (default: 1)
+    :param output: File or stream to output progress to (optional)
     :type directory: str
     :type prefix: str
     :type bucket: :class:`S3Bucket`
-    :type exclude: regex instance
-    :type include: regex instance
+    :type exclude: list
+    :type include: list
     :type concurrency: int
+    :type output: file
 
     """
-    filenames = [list() for i in xrange(concurrency)]
-    count = 0
-    for path, dirs, files in os.walk(directory):
-        for filename in files:
-            filename = os.path.join(path, filename)
-            if include and not include.search(filename):
-                continue
-            if exclude and exclude.search(filename):
-                continue
-            filenames[count % concurrency].append(filename)
-            count += 1
+    def __init__(self, directory, prefix, bucket, include=None, exclude=None,
+            concurrency=1, output=None):
+        self.directory = directory
+        self.prefix = prefix
+        self.bucket = bucket
+        self.include = include
+        self.exclude = exclude
+        self.output = output
+        self.total = 0
+        self.count = 0
+        self.errors = 0
 
-    queues = []
-    for queue in filenames:
-        queue = S3Queue(prefix, queue, bucket, directory)
-        queues.append(queue)
-        queue.start()
+    def upload(self):
+        """
+        Starts the uploading and returns a list of failed filenames.
 
-    failures = []
-    for queue in queues:
-        queue.join()
-        failures.extend(queue.failed)
+        """
+        self.count = 0
+        self.errors = 0
 
-    return failures
+        # Get all the files
+        filenames = self.get_filenames(split=True)
+
+        # Start a queue with each group of files
+        queues = []
+        for queue in filenames:
+            queue = S3Queue(self.prefix, queue, self.bucket, self.directory,
+                    counter=self.counter)
+            queues.append(queue)
+            queue.start()
+
+        # Wait for the queues to all finish
+        failures = []
+        for queue in queues:
+            queue.join()
+            failures.extend(queue.failed)
+
+        return failures
+
+    def get_filenames(self, split=False):
+        """
+        Return a list of filenames to upload, filtered by :attr:`include` and
+        :attr:`exclude`, if set.
+
+        If `split` is ``True``, then this method returns a list of lists, where
+        filenames are evenly divided into :attr:`concurrency` groups.
+
+        After running this method, :attr:`total` will be set to the number of
+        filenames found.
+
+        """
+        filenames = []
+        self.total = 0
+        for path, dirs, files in os.walk(self.directory):
+            for filename in files:
+                filename = os.path.join(path, filename)
+                # Iterate over all the include regexes, determining if we
+                # should include this filename
+                if self.include:
+                    skip = True
+                    for reg in self.include:
+                        if reg.search(filename):
+                            skip = False
+                            break
+                    if skip:
+                        continue
+                # Iterate over the exclude regexes, seeing if we should skip
+                if self.exclude:
+                    skip = False
+                    for reg in self.exclude:
+                        if reg.search(filename):
+                            skip = True
+                            break
+                    if skip:
+                        continue
+                filenames.append(filename)
+                self.total += 1
+
+        if split:
+            # Split into equal size lists for each thread
+            filenames = [filenames[i:i + self.concurrency] for i in range(0,
+                len(filenames), self.concurrency)]
+
+        return filenames
+
+    def counter(self, error=False):
+        """
+        Increment :attr:`count` for each time this is called.
+
+        If `error` is ``True`` then :attr:`errors` is incremented too.
+
+        :param bool error: Indicates there was an error
+
+        """
+        # XXX: We may need a lock around this
+        if error:
+            self.errors += 1
+        self.count += 1
+        self._output()
+
+    def _output(self):
+        """
+        Print the current progress.
+
+        """
+        if not self.output:
+            # Exit out if we don't have a stream to output to
+            return
+
+        # Get the total count as a string, so we can find its length
+        total = str(self.total)
+        # Compose a format specifier using the length of the total
+        count = "{: " + str(len(total)) + "d}"
+        # Format the count nicely with our specifier, which pads with spaces
+        count = count.format(self.count)
+        # Compose our whole line
+        line = count + "/" + total + " files copied"
+
+        # Add the error count if we have one
+        if self.errors:
+            line += " ({} error{})".format(self.errors,
+                    self.errors > 1 and 's' or '')
+
+        # Add spacing to blot out the rest of the line
+        line += " " * (int(os.environ.get('COLUMNS', 80)) - len(line) - 1)
+
+        # Write the line to the stream, using \r to start us at the beginning
+        self.output.write('\r' + line + '\r')
+
+        # Flush the stream if that's possible
+        if hasattr(self.output, 'flush'):
+            self.output.flush()
 
 
+def sync_to_s3(directory, prefix, bucket, include=None, exclude=None,
+        concurrency=1, output=None):
+    """
+    Uploads `directory` to S3 with `prefix` to the key names in `bucket` and
+    return a list of failed filenames.
+
+    :param directory: Directory to sync
+    :param prefix: S3 key prefix
+    :param bucket: A :class:`S3Bucket` instance
+    :param exclude: List of filename regexes to exclude (optional)
+    :param include: List of filename regexes to include (optional)
+    :param concurrency: Number of concurrent uploads to use (default: 1)
+    :param output: File or stream to output progress to (optional)
+    :type directory: str
+    :type prefix: str
+    :type bucket: :class:`S3Bucket`
+    :type exclude: list
+    :type include: list
+    :type concurrency: int
+    :type output: file
+
+    """
+    uploader = S3Uploader(directory, prefix, bucket, include, exclude,
+            concurrency, output)
+    return uploader.upload()
 
